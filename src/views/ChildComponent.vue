be8da032a53f83c2006481d9b2092f4a44ff38c3
Faizan Siddiqui <siddiquifaizan248@gamil.com>
2022-08-31 00:29:17 +0530

    First commit

---
+++ src/views/ChildComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="childCSS">
+        <h1>Child Component</h1>
+        <h1>{{text}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    props:['ChildCalling'],
+    data() {
+        return {
+            text: 'Click to change text'
+        }
+    },
+    watch: {
+        ChildCalling(newVal) {
+            this.changeText(newVal);
+        }
+    },
+    methods: {
+        changeText(newVal) {
+            this.text =newVal ;
+        }
+
+    }
+}
+</script>
+<style scoped>
+.childCSS{
+    background-color: #42b983;
+    padding: 20px;
+    color: #fff;
+    text-align: center;
+}
+</style>
---
+++ src/views/ParentComponent.vue
@@ -0,0 +1,32 @@
+
+<template>
+    <div>
+        <h1>Parent</h1>
+        <Child :ChildCalling="ParentText"></Child>
+        <button @click="childCalling">Click me</button>
+        <input type="text" v-model.trim="inputText">
+    </div>
+</template>
+<script>
+/* eslint-disable */
+import Child from './ChildComponent.vue'
+export default {
+    data(){
+        return {
+            ParentText: 'Click to change text',
+            inputText:"",
+        }
+    
+    },
+    components:{
+        Child
+    },
+    methods:{
+        childCalling(){
+            console.log(this.inputText);  
+            this.ParentText=this.inputText;
+        }
+    }
+    
+}
+</script>
